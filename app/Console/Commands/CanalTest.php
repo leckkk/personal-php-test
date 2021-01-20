2fe437371e495f5de03d2e83553b4817084d269f
leck <lichuankai@linhuiba.com>
2021-01-20 20:09:36 +0800

    init

---
+++ app/Console/Commands/CanalTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use xingwenge\canal_php\CanalClient;
+use xingwenge\canal_php\CanalConnectorFactory;
+use xingwenge\canal_php\Fmt;
+
+class CanalTest extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'canal:listen';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '测试canal';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        try {
+            $client = CanalConnectorFactory::createClient(CanalClient::TYPE_SOCKET_CLUE);
+            # $client = CanalConnectorFactory::createClient(CanalClient::TYPE_SWOOLE);
+
+            //canal-server 监听地址端口
+            $client->connect("192.168.0.152", 11111);
+            $client->checkValid();
+            //订阅binlog信息 与canal-server配置一致
+            $client->subscribe("1001", "example", "linhuiba\\.users");
+            # $client->subscribe("1001", "example", "db_name.tb_name"); # 设置过滤
+
+            while (true) {
+                $message = $client->get(100);
+                if ($entries = $message->getEntries()) {
+                    foreach ($entries as $entry) {
+                        Fmt::println($entry);
+                    }
+                }
+                sleep(1);
+            }
+
+            $client->disConnect();
+        } catch (\Exception $e) {
+            echo $e->getMessage(), PHP_EOL;
+        }
+    }
+}
