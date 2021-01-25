ee3ff1e9d84ba0427b308faa7d16bf6112debac3
leck <lichuankai@linhuiba.com>
2021-01-25 19:11:19 +0800

    修改配置项

--- app/Console/Commands/ScreenshotsTest.php
+++ app/Console/Commands/ScreenshotsTest.php
@@ -45,8 +45,8 @@ class ScreenshotsTest extends Command
             $this->info('请输入视频流地址 {--url=} ');
         }
         $ffmpeg = FFMpeg::create([
-            'ffmpeg.binaries'  => exec('which ffmpeg'),
-            'ffprobe.binaries' => exec('which ffprobe'),
+            'ffmpeg.binaries'  => '/usr/bin/ffmpeg',
+            'ffprobe.binaries' => '/usr/bin/ffprobe',
             'timeout'          => 3600, // The timeout for the underlying process
             'ffmpeg.threads'   => 12,   // The number of threads that FFMpeg should use
         ]);
