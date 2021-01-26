0f6914d2a37e4784080aa2f4bdb98bb50016e81b
leck <lichuankai@linhuiba.com>
2021-01-26 14:09:35 +0800

    修改超时时间

--- app/Console/Commands/ScreenshotsTest.php
+++ app/Console/Commands/ScreenshotsTest.php
@@ -49,7 +49,7 @@ class ScreenshotsTest extends Command
         $ffmpeg = FFMpeg::create([
             'ffmpeg.binaries'  => '/usr/bin/ffmpeg',
             'ffprobe.binaries' => '/usr/bin/ffprobe',
-            'timeout'          => 3600, // The timeout for the underlying process
+            'timeout'          => 180, // The timeout for the underlying process
             'ffmpeg.threads'   => 12,   // The number of threads that FFMpeg should use
         ]);
 
