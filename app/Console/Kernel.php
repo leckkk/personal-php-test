31d31d638cfea234b14c46fd36e0b0e193205887
leck <lichuankai@linhuiba.com>
2021-01-27 16:37:18 +0800

    调整截图时间

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
 
-        $schedule->command('test:screenshots')->everyThreeMinutes();
+        $schedule->command('test:screenshots')->everyFiveMinutes();
     }
 
     /**
